90f477aef107968d17ba2724171f1019abb46147
Gabriel <gabrielpinango1492@gmail.com>
2018-04-04 21:55:24 -0300

    Added loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,9 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-
+$(window).load(function () {
+    $('.loading').delay(7000).fadeOut('slow');
+});
 (function($) {
 
 	skel
